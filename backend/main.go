f9976cb4f18ab0b9065260671d29c12306a6a81b
Tony Lee <tungsheng@gmail.com>
2018-06-12 04:40:51 +0800

    update backend, add say client

--- backend/main.go
+++ backend/main.go
@@ -1,26 +1,57 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"net"
+	"os/exec"
 
-	"github.ocm/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus"
+	pb "github.com/tungsheng/say-grpc/api"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	port := flag.Int("p", 8080, "port to listen to")
-	flag.pause
+	flag.Parse()
 
-	lis, err = net.Listen("tcp", fmt.Sprintg("%d", *port))
+	logrus.Infof("listening to port %v", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		logrus.Fatalf("could not listen to port %v: $v", *port, err)
+		logrus.Fatalf("could not listen to port %d: $v", *port, err)
 	}
 
 	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
+	err = s.Serve(lis)
+	if err != nil {
+		logrus.Fatalf("could not serve: %v", err)
+	}
+
 }
 
-//cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-//cmd.Stdout = os.Stdout
-//cmd.Stderr = os.Stderr
-//if err := cmd.Run(); err != nil {
-//log.Fatal(err)
-//}
+type server struct{}
+
+func (s server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("could not create temp file: %v", err)
+	}
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close %s: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite failed: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("could not read temp file: %v", err)
+	}
+
+	return &pb.Speech{Audio: data}, nil
+}
---
+++ say/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/tungsheng/say-grpc/api"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	backend := flag.String("b", "localhost:8080", "address of the say backend")
+	output := flag.String("o", "output.wav", "wav file where the output will be written")
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Printf("usage:\n\t%s \"text to speech\"\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	conn, err := grpc.Dial(*backend, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect to %s: %v", *backend, err)
+	}
+	defer conn.Close()
+
+	client := pb.NewTextToSpeechClient(conn)
+
+	text := &pb.Text{Text: os.Args[1]}
+	res, err := client.Say(context.Background(), text)
+	if err != nil {
+		log.Fatalf("could not say %s: %v", text.Text, err)
+	}
+
+	if err := ioutil.WriteFile(*output, res.Audio, 0666); err != nil {
+		log.Fatalf("could not write to %s: %v", *output, err)
+	}
+
+}
